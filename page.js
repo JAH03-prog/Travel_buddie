df552a164abe14b14e017c71818476af811c019a
Jah-Vin James <130305430+JAH03-prog@users.noreply.github.com>
2024-09-02 21:37:33 -0400

    Add files via upload

---
+++ page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import {box} from '@mui/material'
+
+export default function Home() {
+  return (
+    
+    <div className = 'main'>
+      <header>Hello</header>
+      <main></main>
+    </div>
+    
+  );
+}
